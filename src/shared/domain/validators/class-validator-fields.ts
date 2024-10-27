21611c1075d56a560cfec48c4e3aa9d0ef528773
Lorrayne Costa de Carvalho <lorrayne.carvalho@ibm.com>
2024-10-27 11:29:56 -0300

    feat: validation tests unit

--- src/shared/domain/validators/class-validator-fields.ts
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -1,27 +1,26 @@
 import { validateSync } from 'class-validator'
 import {
-  FieldsErrors,
-  ValidatorFieldsInterface,
+ FieldsErrors,
+ ValidatorFieldsInterface,
 } from './validator-fields.interface'
 
 export abstract class ClassValidatorFields<PropsValidated>
-  implements ValidatorFieldsInterface<PropsValidated>
+ implements ValidatorFieldsInterface<PropsValidated>
 {
-  errors: FieldsErrors = null
-  validatedData: PropsValidated = null
-  validate(data: any): boolean {
-    const errors = validateSync(data)
+ errors: FieldsErrors = null
+ validatedData: PropsValidated = null
 
-    if (errors.length) {
-      this.errors = {}
-      for (const error of errors) {
-        const field = error.property
-        this.errors[field] = Object.values(error.constraints)
-      }
-    } else {
-      this.validatedData = data
-    }
-
-    return !errors.length
+ validate(data: any): boolean {
+  const errors = validateSync(data)
+  if (errors.length) {
+   this.errors = {}
+   for (const error of errors) {
+    const field = error.property
+    this.errors[field] = Object.values(error.constraints)
+   }
+  } else {
+   this.validatedData = data
   }
+  return !errors.length
+ }
 }
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,50 +1,59 @@
 import { Entity } from '@/shared/domain/entities/entity'
+import { UserValidatorFactory } from '../validators/user.validator'
 
 export type UserProps = {
-  name: string
-  email: string
-  password: string
-  createdAt?: Date
+ name: string
+ email: string
+ password: string
+ createdAt?: Date
 }
 
 export class UserEntity extends Entity<UserProps> {
-  constructor(
-    public readonly props: UserProps,
-    id?: string,
-  ) {
-    super(props, id)
-    this.props.createdAt = this.props.createdAt ?? new Date()
-  }
+ constructor(
+  public readonly props: UserProps,
+  id?: string,
+ ) {
+  UserEntity.validate(props)
+  super(props, id)
+  this.props.createdAt = this.props.createdAt ?? new Date()
+ }
 
-  update(value: string): void {
-    this.name = value
-  }
+ update(value: string): void {
+  UserEntity.validate({ ...this.props, name: value })
+  this.name = value
+ }
 
-  updatePassword(value: string): void {
-    this.password = value
-  }
+ updatePassword(value: string): void {
+  UserEntity.validate({ ...this.props, password: value })
+  this.password = value
+ }
 
-  get name() {
-    return this.props.name
-  }
+ get name() {
+  return this.props.name
+ }
 
-  private set name(value: string) {
-    this.props.name = value
-  }
+ private set name(value: string) {
+  this.props.name = value
+ }
 
-  get email() {
-    return this.props.email
-  }
+ get email() {
+  return this.props.email
+ }
 
-  get password() {
-    return this.props.password
-  }
+ get password() {
+  return this.props.password
+ }
 
-  private set password(value: string) {
-    this.props.password = value
-  }
+ private set password(value: string) {
+  this.props.password = value
+ }
 
-  get createdAt() {
-    return this.props.createdAt
-  }
+ get createdAt() {
+  return this.props.createdAt
+ }
+
+ static validate(props: UserProps) {
+  const validator = UserValidatorFactory.create()
+  validator.validate(props)
+ }
 }
